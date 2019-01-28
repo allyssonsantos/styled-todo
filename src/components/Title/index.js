de3eb939fa8ae8d9922df1590a6845176f26f298
Allysson Santos <aallysson0@gmail.com>
2019-01-28 21:32:59 -0200

    Create Title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #abb7b7;
+  font-size: 28px;
+  margin: 0;
+  text-align: center;
+`;
+
+export default Title;
