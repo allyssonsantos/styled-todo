65364808ce3a35353779893ea67f73102f8c0c58
Allysson Santos <aallysson0@gmail.com>
2019-01-28 21:32:54 -0200

    Add style

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  border-bottom-right-radius: 0;
+  border-top-right-radius: 0;
+  box-sizing: border-box;
+  font-size: 14px;
+  padding: 10px;
+
+  width: 90%;
+`;
+
+const Button = styled.button`
+  border: 1px solid #abb7b7;
+  border-radius: 3px;
+  border-bottom-left-radius: 0;
+  border-top-left-radius: 0;
+  color: #fff;
+  cursor: pointer;
+  background-color: #abb7b7;
+  font-size: 14px;
+  padding: 10px;
+
+  width: 10%;
+`;
 
 const Form = ({ handleSubmit }) => (
   <form onSubmit={handleSubmit}>
-    <input name="todo" type="text" />
-    <button type="Submit">Adicionar</button>
+    <Input name="todo" type="text" autoComplete="off" />
+    <Button type="Submit">+</Button>
   </form>
 );
 
